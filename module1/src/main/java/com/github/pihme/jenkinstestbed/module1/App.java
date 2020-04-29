a1a22a522bbe77c6beccf2fab53c756343bf5386
pihme <pihme@users.noreply.github.com>
2020-04-29 16:09:02 +0200

    add maven skeleton

---
+++ module1/src/main/java/com/github/pihme/jenkinstestbed/module1/App.java
@@ -0,0 +1,13 @@
+package com.github.pihme.jenkinstestbed.module1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
