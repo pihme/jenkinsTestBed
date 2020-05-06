fa217cbe6bb344218e87ca8fcc60503fe25bd845
pihme <pihme@users.noreply.github.com>
2020-05-06 13:36:14 +0200

    detect flaky tests and exit with error

--- runTests.sh
+++ runTests.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-mvn test -Dsurefire.rerunFailingTestsCount=15
+mvn test -Dsurefire.rerunFailingTestsCount=15 | tee test.txt
+
+if grep -q "\[WARNING\] Flakes:" "test.txt"; then
+  
+
+  echo ERROR: Flaky Tests detected>&2
+  exit 1
+fi
